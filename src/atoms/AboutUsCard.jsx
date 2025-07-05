2d69bf369d1d6974d31a54fe2a2730772ec35594
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-07-05 22:46:00 +0200

    add atom AboutUsCard

---
+++ src/atoms/AboutUsCard.jsx
@@ -0,0 +1,20 @@
+export default function AboutUsCard(props) {
+    return <>
+        <div className={`container-xxl my-5 ${props.image}`}>
+            <div className="container">
+                <div className="row g-0">
+                    {props.direction === 'left'? <div className="col-lg-6"></div>:<></>}
+                    <div className="col-lg-6 pt-lg-5 wow fadeIn" data-wow-delay="1s">
+                        <div className="bg-white rounded-top p-5 mt-lg-5">
+                            <h1 className="display-6 mb-4 fw-bolder">About US</h1>
+                            <h5 className="mb-4 text-black-50 text-center">
+                                {props.descrition}
+                            </h5>
+                            <a className="btn btn-primary rounded-pill py-3 px-5" href={props.link}>Explore More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,12 @@
+import AboutUsCard from "../atoms/AboutUsCard";
+
 export default function About() {
     return (
-        <div className="container-xxl about my-5">
-            <div className="container">
-                <div className="row g-0">
-                    <div className="col-lg-6"></div>
-                    <div className="col-lg-6 pt-lg-5 wow fadeIn" data-wow-delay="1s">
-                        <div className="bg-white rounded-top p-5 mt-lg-5">
-                            <h1 className="display-6 mb-4 fw-bolder">About US</h1>
-                            <h5 className="mb-4 text-black-50 text-center">
-                                ENSIT Student Branch, founded in 2013 and has been active since. Now it gathers over 120 members and to mention the number is increasing continuously. We believe in IEEE’s causes and values. Fostering Technological innovation for the benefit of humanity is a mission we want to take part of. Enhancing our soft skills and enlarging our technical knowledge for better tomorrow engineers and searchers, a better Tunisia, a better world, that is our vision.
-                            </h5>
-                            <a className="btn btn-primary rounded-pill py-3 px-5" href="https://www.ieee.org/">Explore More</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <AboutUsCard
+            direction="left"
+            image="about"
+            link="https://www.ieee.org/"
+            descrition="ENSIT Student Branch, founded in 2013 and has been active since. Now it gathers over 120 members and to mention the number is increasing continuously. We believe in IEEE’s causes and values. Fostering Technological innovation for the benefit of humanity is a mission we want to take part of. Enhancing our soft skills and enlarging our technical knowledge for better tomorrow engineers and searchers, a better Tunisia, a better world, that is our vision."
+        />
     );
 }
