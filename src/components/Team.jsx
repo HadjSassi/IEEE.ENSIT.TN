ef8038e2e2887660a1287a2d669d7c896b9e5169
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-07-06 15:59:33 +0200

    extract data from teams

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,59 +1,23 @@
 import SectionTitle from "../atoms/SectionTitle";
 import TeamItem from "../molecules/TeamItem";
 
-export default function Team() {
+export default function Team({data}) {
     return (
         <div id="teamindex" className="container-xxl py-5 d-flex justify-content-center">
             <div className="container">
-                <SectionTitle heading={'2023-2024 Mandate'} subHeading={'our Team'} />
+                <SectionTitle heading={'2023-2024 Mandate'} subHeading={'our Team'}/>
                 <div className="row g-4 justify-content-center">
-                    <TeamItem
-                        image="img/sb/elanoubli%20chair%20sb.jpg"
-                        name="Elyanoubli Mohamed Amine"
-                        post="Chairman"
-                    />
-                    <TeamItem
-                        image="img/sb/molka%20vp%20sb.jpg"
-                        name="Guesmi Molka"
-                        post="Vice-Chair"
-                        facebook="https://www.facebook.com/profile.php?id=100009455581309"
-                        instagram="https://www.instagram.com/molka.guesmi"
-                        linkedin="https://www.linkedin.com/in/molka-guesmi-112036220"
-                    />
-                    <TeamItem
-                        image="img/sb/ilef%20sg%20sb.jpg"
-                        name="Zeiri Ilef"
-                        post="General Secretary"
-                        facebook="https://www.facebook.com/ilef.zeiri"
-                        instagram="https://www.instagram.com/ilefzeiri/"
-                        linkedin="https://www.linkedin.com/in/ilef-zeiri-4821a1253/"
-                    />
-                    <TeamItem
-                        image="img/sb/khalaf%20rh%20sb.jpg"
-                        name="Nakbi Khalaf"
-                        post="HR Manager"
-                    />
-                    <TeamItem
-                        image="img/sb/mahdi.jpeg"
-                        name="Hadj Sassi Mahdi"
-                        post="Web Manager"
-                        facebook="https://www.facebook.com/HadjSassiMahdiEzzedine"
-                        instagram="https://www.instagram.com/hadjsassi.ezzedine.mahdi/"
-                        linkedin="https://www.linkedin.com/in/mahdi-hadj-sassi/"
-                    />
-                    <TeamItem
-                        image="img/sb/aziz%20web%20sb.jpg"
-                        name="Guebsi Mohamed Aziz"
-                        post="Webmaster"
-                        facebook="https://www.facebook.com/aziz.zizoo.7?mibextid=D4KYlr"
-                        instagram="https://instagram.com/aziz_gabsi_?igshid=MmU2YjMzNjRlOQ=="
-                        linkedin="https://www.linkedin.com/in/gabsi-aziz-110490242"
-                    />
-                    <TeamItem
-                        image="img/sb/oussema%20treso%20sb.jpg"
-                        name="Touhemi Oussema"
-                        post="Treasurer"
-                    />
+                    {data.map((member, index) => (
+                        <TeamItem
+                            key={index}
+                            image={member.image}
+                            name={member.name}
+                            post={member.post}
+                            facebook={member.facebook}
+                            instagram={member.instagram}
+                            linkedin={member.linkedin}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,37 @@
 import Carrousel from "../components/Carrousel";
 import Statistics from "../components/Statistics";
-import About from "../components/About";
 import Units from "../components/Units";
 import Team from "../components/Team";
 import JoinUs from "../components/JoinUs";
 import Dedicace from "../components/Dedicace";
 import Footer from "../components/Footer";
+import AboutUsCard from "../molecules/AboutUsCard";
+import React, {useEffect, useState} from "react";
 
 export default function Home() {
+    const [teamData, setTeamData] = useState(null);
+    useEffect(() => {
+        fetch("/data/sb_mandate.json")
+            .then((response) => response.json())
+            .then((data) => setTeamData(data))
+            .catch((error) => console.error("Erreur lors du chargement des données :", error));
+    }, []);
+
+    if (!teamData) {
+        return <div>Chargement...</div>;
+    }
     return (
         <div>
             <Carrousel />
             <Statistics/>
-            <About/>
+            <AboutUsCard
+                direction="left"
+                image="about"
+                link="https://www.ieee.org/"
+                descrition="ENSIT Student Branch, founded in 2013 and has been active since. Now it gathers over 120 members and to mention the number is increasing continuously. We believe in IEEE’s causes and values. Fostering Technological innovation for the benefit of humanity is a mission we want to take part of. Enhancing our soft skills and enlarging our technical knowledge for better tomorrow engineers and searchers, a better Tunisia, a better world, that is our vision."
+            />
             <Units/>
-            <Team/>
+            <Team data={teamData} />
             <JoinUs/>
             <Dedicace/>
             <Footer />
